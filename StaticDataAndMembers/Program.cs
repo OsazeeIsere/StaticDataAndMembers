75ef7b93fe7c174d076387d5e75e4dbfae2855ec
Osazee Isere <oz1.concepts@gmail.com>
2021-06-17 04:03:16 +0100

    working with static data and members

--- StaticDataAndMembers/Program.cs
+++ StaticDataAndMembers/Program.cs
@@ -10,6 +10,29 @@ namespace StaticDataAndMembers
     {
         static void Main(string[] args)
         {
+           // Console.SetBufferSize(400, 1200);
+            Console.SetWindowSize(80, 50);
+            Console.WriteLine("=> Fun with Static Data \n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            //print the current interest rate
+            Console.WriteLine("Interest Rate is:{0}", SavingsAccount.GetInterestRate());
+
+            //make a new object. this does not reset the interest rate.
+            SavingsAccount s3 = new SavingsAccount(1000.75);
+            Console.WriteLine("Interest Rate is:{0}", SavingsAccount.GetInterestRate());
+            
+            //try to change the interest rate via property.
+            SavingsAccount.SetIntterestRate(0.08);
+            SavingsAccount s2 = new SavingsAccount(100);
+
+            //should print out 0.08....right??
+            Console.WriteLine("Interest Rate After trying to reset the interest rate is:{0}", SavingsAccount.GetInterestRate());
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
---
+++ StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        //instance-level data.
+        public double currBalance;
+        //A static point of data
+        public static double currInterestRate;
+        //custom constructor
+        public SavingsAccount(double balance)
+        {
+            currInterestRate = 0.04;//this is static data
+            currBalance = balance;
+        }
+        //A static constructor
+         static  SavingsAccount()
+        {
+            Console.WriteLine("In static ctor!");
+            currInterestRate = 0.04;//this is static data
+        }
+        //creating  static methods
+        public static double GetInterestRate() => currInterestRate; //expression-bodied method
+        public static void SetIntterestRate(double newRate) => currInterestRate = newRate;
+    }
+}
